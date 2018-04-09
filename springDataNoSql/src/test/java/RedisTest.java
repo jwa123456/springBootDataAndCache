19fccba0cbf44456ba8d7666ef0df87cf634619d
wei.jiang <wei.jiang@17zuoye.com>
2018-04-09 20:58:14 +0800

    add nosql project

---
+++ springDataNoSql/src/test/java/RedisTest.java
@@ -0,0 +1,46 @@
+import com.job.Application;
+import com.job.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class RedisTest {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private RedisTemplate<String, User> redisTemplate;
+
+	@Test
+	public void test() throws Exception {
+
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+		// 保存对象
+		User user = new User("abc", 20);
+		redisTemplate.opsForValue().set(user.getUsername(), user);
+
+		user = new User("aaa", 30);
+		redisTemplate.opsForValue().set(user.getUsername(), user);
+
+		user = new User("bbb", 40);
+		redisTemplate.opsForValue().set(user.getUsername(), user);
+
+		Assert.assertEquals(20, redisTemplate.opsForValue().get("abc").getAge().longValue());
+		Assert.assertEquals(30, redisTemplate.opsForValue().get("aaa").getAge().longValue());
+		Assert.assertEquals(40, redisTemplate.opsForValue().get("bbb").getAge().longValue());
+
+	}
+
+}
